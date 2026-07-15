4e9adf72ba2aa39d15c5a5c9949bb2c591b3a41a
RohanMishra47 <mydearluffy093@gmail.com>
2026-07-15 10:45:23 +0530

    refactor SQL query in export_powerbi_data.py to enhance data extraction for fact orders, including shipping type and delivery delay analysis

--- analysis/export_powerbi_data.py
+++ analysis/export_powerbi_data.py
@@ -43,57 +43,146 @@ Project Pipeline:
 from helper.utils import run_query
 
 sql1 = """
+WITH orders_clean AS (
+
+    SELECT
+        o.order_id,
+        o.order_status,
+
+        o.order_purchase_timestamp,
+        o.order_approved_at,
+        o.order_delivered_customer_date,
+        o.order_estimated_delivery_date,
+
+        DATE(o.order_purchase_timestamp) AS order_date,
+        strftime('%Y-%m', o.order_purchase_timestamp) AS order_month,
+
+        c.customer_id,
+        c.customer_unique_id,
+        c.customer_city,
+        c.customer_state,
+
+        oi.seller_id,
+        s.seller_city,
+        s.seller_state,
+
+        oi.product_id,
+        COALESCE(
+            pct.product_category_name_english,
+            p.product_category_name
+        ) AS product_category,
+
+        ROUND(oi.price, 2) AS price,
+        ROUND(oi.freight_value, 2) AS freight_value,
+        ROUND(oi.price + oi.freight_value, 2) AS gmv,
+
+        ROUND(
+            (oi.freight_value / NULLIF(oi.price, 0)) * 100,
+            2
+        ) AS freight_percentage,
+
+        r.review_score,
+
+        CAST(
+            julianday(DATE(o.order_delivered_customer_date)) -
+            julianday(DATE(o.order_estimated_delivery_date))
+            AS INTEGER
+        ) AS delivery_delay_days
+
+    FROM olist_orders_dataset AS o
+
+    JOIN olist_customers_dataset AS c
+        ON o.customer_id = c.customer_id
+
+    JOIN olist_order_items_dataset AS oi
+        ON o.order_id = oi.order_id
+
+    JOIN olist_products_dataset AS p
+        ON oi.product_id = p.product_id
+
+    LEFT JOIN product_category_name_translation AS pct
+        ON p.product_category_name = pct.product_category_name
+
+    JOIN olist_sellers_dataset AS s
+        ON oi.seller_id = s.seller_id
+
+    LEFT JOIN olist_order_reviews_dataset AS r
+        ON o.order_id = r.order_id
+
+    WHERE o.order_status = 'delivered'
+)
+
 SELECT
-    o.order_id,
-    DATE(o.order_purchase_timestamp) AS order_date,
-    strftime('%Y-%m', o.order_purchase_timestamp) AS order_month,
+    order_id,
+    order_status,
 
-    c.customer_id,
-    c.customer_unique_id,
-    c.customer_state,
+    order_purchase_timestamp,
+    order_approved_at,
+    order_delivered_customer_date,
+    order_estimated_delivery_date,
 
-    oi.seller_id,
-    s.seller_state,
+    order_date,
+    order_month,
 
-    oi.product_id,
-    p.product_category_name,
+    customer_id,
+    customer_unique_id,
+    customer_city,
+    customer_state,
 
-    ROUND(oi.price, 2) AS product_price,
-    ROUND(oi.freight_value, 2) AS freight_value,
-    ROUND(oi.price + oi.freight_value, 2) AS gmv,
+    seller_id,
+    seller_city,
+    seller_state,
 
-    ROUND(
-        ((oi.freight_value / NULLIF(oi.price, 0)) * 100),
-        2
-    ) AS freight_percentage_of_price,
+    CASE
+        WHEN customer_state = seller_state
+        THEN 'Intra-State'
+        ELSE 'Inter-State'
+    END AS shipping_type,
 
-    r.review_score,
+    product_id,
+    product_category,
 
-    CAST(
-        julianday(DATE(o.order_delivered_customer_date)) -
-        julianday(DATE(o.order_estimated_delivery_date))
-        AS INTEGER
-    ) AS delivery_delay_days
+    price,
+    freight_value,
+    gmv,
+    freight_percentage,
 
-FROM olist_orders_dataset AS o
+    review_score,
+    delivery_delay_days,
 
-JOIN olist_customers_dataset AS c
-    ON o.customer_id = c.customer_id
+    CASE
+        WHEN delivery_delay_days > 0 THEN 1
+        ELSE 0
+    END AS late_delivery_flag,
 
-JOIN olist_order_items_dataset AS oi
-    ON o.order_id = oi.order_id
+    CASE
+        WHEN (
+            delivery_delay_days > 100
+            AND review_score = 5
+        )
+        OR (
+            delivery_delay_days < -100
+            AND review_score = 1
+        )
+        THEN 1
+        ELSE 0
+    END AS delivery_delay_outlier_flag,
 
-JOIN olist_products_dataset AS p
-    ON oi.product_id = p.product_id
+    CASE
+        WHEN
+            delivery_delay_days > 100
+            AND review_score = 5
+        THEN 'Late >100 days with 5-star review'
 
-JOIN olist_sellers_dataset AS s
-    ON oi.seller_id = s.seller_id
+        WHEN
+            delivery_delay_days < -100
+            AND review_score = 1
+        THEN 'Early <-100 days with 1-star review'
 
-LEFT JOIN olist_order_reviews_dataset AS r
-    ON o.order_id = r.order_id
+        ELSE 'None'
+    END AS delivery_delay_outlier_reason
 
-WHERE o.order_status = 'delivered';
-"""
+FROM orders_clean;"""
 run_query("Fact Orders Export", sql1, export_csv=True, filename="fact_orders_clean.csv")
 
 sql2 = """
